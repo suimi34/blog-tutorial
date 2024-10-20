4c8633ad55370aa95c2672d277b3dc1e74895a80
Susumu Ishikawa <susumu2934@gmail.com>
2024-10-20 13:07:04 +0900

    pulling from data source

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,17 +1,10 @@
+import { prisma } from "~/db.server";
+
 interface Post {
   slug: string;
   title: string;
 }
 
 export async function getPosts(): Promise<Post[]> {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+  return prisma.post.findMany();
 }
